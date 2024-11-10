8397ca436dac88157a49979a8050153b31ca73b9
Milwad <milwad.dev@gmail.com>
2024-11-10 22:48:57 +0330

    install and configure axios

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 const app = createApp(App)
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+axios.interceptors.request.use(config => {
+    config.headers['Accept'] = 'application/json';
+
+    return config;
+}, error => Promise.reject(error));
 
 app.use(createPinia())
 app.use(router)
