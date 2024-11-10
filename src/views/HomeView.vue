2669dab2fafaad9322052f147c12f70d6171ab2d
Milwad <milwad.dev@gmail.com>
2024-11-10 21:39:50 +0330

    initial view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,234 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+window.onload = function () {
+    bootlint.showLintReportForCurrentDocument([], {
+        hasProblems: false,
+        problemFree: false
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    function formatDate(date) {
+        return (
+            date.getDate() +
+            "/" +
+            (date.getMonth() + 1) +
+            "/" +
+            date.getFullYear()
+        );
+    }
+
+    var currentDate = formatDate(new Date());
+
+    $(".due-date-button").datepicker({
+        format: "dd/mm/yyyy",
+        autoclose: true,
+        todayHighlight: true,
+        startDate: currentDate,
+        orientation: "bottom right"
+    });
+
+    $(".due-date-button").on("click", function (event) {
+        $(".due-date-button")
+            .datepicker("show")
+            .on("changeDate", function (dateChangeEvent) {
+                $(".due-date-button").datepicker("hide");
+                $(".due-date-label").text(formatDate(dateChangeEvent.date));
+            });
+    });
+};
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container m-5 p-2 rounded mx-auto bg-light shadow">
+    <!-- App title section -->
+    <div class="row m-1 p-4">
+        <div class="col">
+            <div class="p-1 h1 text-primary text-center mx-auto display-inline-block">
+                <i class="fa fa-check bg-primary text-white rounded p-2"></i>
+                <u>My Todo-s</u>
+            </div>
+        </div>
+    </div>
+    <!-- Create todo section -->
+    <div class="row m-1 p-3">
+        <div class="col col-11 mx-auto">
+            <div class="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
+                <div class="col">
+                    <input class="form-control form-control-lg border-0 add-todo-input bg-transparent rounded" type="text" placeholder="Add new ..">
+                </div>
+                <div class="col-auto m-0 px-2 d-flex align-items-center">
+                    <label class="text-secondary my-2 p-0 px-1 view-opt-label due-date-label d-none">Due date not set</label>
+                    <i class="fa fa-calendar my-2 px-1 text-primary btn due-date-button" data-toggle="tooltip" data-placement="bottom" title="Set a Due date"></i>
+                    <i class="fa fa-calendar-times-o my-2 px-1 text-danger btn clear-due-date-button d-none" data-toggle="tooltip" data-placement="bottom" title="Clear Due date"></i>
+                </div>
+                <div class="col-auto px-0 mx-0 mr-2">
+                    <button type="button" class="btn btn-primary">Add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="p-2 mx-4 border-black-25 border-bottom"></div>
+    <!-- View options section -->
+    <div class="row m-1 p-3 px-5 justify-content-end">
+        <div class="col-auto d-flex align-items-center">
+            <label class="text-secondary my-2 pr-2 view-opt-label">Filter</label>
+            <select class="custom-select custom-select-sm btn my-2">
+                <option value="all" selected>All</option>
+                <option value="completed">Completed</option>
+                <option value="active">Active</option>
+                <option value="has-due-date">Has due date</option>
+            </select>
+        </div>
+        <div class="col-auto d-flex align-items-center px-1 pr-3">
+            <label class="text-secondary my-2 pr-2 view-opt-label">Sort</label>
+            <select class="custom-select custom-select-sm btn my-2">
+                <option value="added-date-asc" selected>Added date</option>
+                <option value="due-date-desc">Due date</option>
+            </select>
+            <i class="fa fa fa-sort-amount-asc text-info btn mx-0 px-0 pl-1" data-toggle="tooltip" data-placement="bottom" title="Ascending"></i>
+            <i class="fa fa fa-sort-amount-desc text-info btn mx-0 px-0 pl-1 d-none" data-toggle="tooltip" data-placement="bottom" title="Descending"></i>
+        </div>
+    </div>
+    <!-- Todo list section -->
+    <div class="row mx-1 px-5 pb-3 w-80">
+        <div class="col mx-auto">
+            <!-- Todo Item 1 -->
+            <div class="row px-3 align-items-center todo-item rounded">
+                <div class="col-auto m-1 p-0 d-flex align-items-center">
+                    <h2 class="m-0 p-0">
+                        <i class="fa fa-square-o text-primary btn m-0 p-0 d-none" data-toggle="tooltip" data-placement="bottom" title="Mark as complete"></i>
+                        <i class="fa fa-check-square-o text-primary btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Mark as todo"></i>
+                    </h2>
+                </div>
+                <div class="col px-1 m-1 d-flex align-items-center">
+                    <input type="text" class="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3" readonly value="Buy groceries for next week" title="Buy groceries for next week" />
+                    <input type="text" class="form-control form-control-lg border-0 edit-todo-input rounded px-3 d-none" value="Buy groceries for next week" />
+                </div>
+                <div class="col-auto m-1 p-0 px-3 d-none">
+                </div>
+                <div class="col-auto m-1 p-0 todo-actions">
+                    <div class="row d-flex align-items-center justify-content-end">
+                        <h5 class="m-0 p-0 px-2">
+                            <i class="fa fa-pencil text-info btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
+                        </h5>
+                        <h5 class="m-0 p-0 px-2">
+                            <i class="fa fa-trash-o text-danger btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
+                        </h5>
+                    </div>
+                    <div class="row todo-created-info">
+                        <div class="col-auto d-flex align-items-center pr-2">
+                            <i class="fa fa-info-circle my-2 px-2 text-black-50 btn" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
+                            <label class="date-label my-2 text-black-50">28th Jun 2020</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Todo Item 2 -->
+            <div class="row px-3 align-items-center todo-item rounded">
+                <div class="col-auto m-1 p-0 d-flex align-items-center">
+                    <h2 class="m-0 p-0">
+                        <i class="fa fa-square-o text-primary btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Mark as complete"></i>
+                        <i class="fa fa-check-square-o text-primary btn m-0 p-0 d-none" data-toggle="tooltip" data-placement="bottom" title="Mark as todo"></i>
+                    </h2>
+                </div>
+                <div class="col px-1 m-1 d-flex align-items-center">
+                    <input type="text" class="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3" readonly value="Renew car insurance" title="Renew car insurance" />
+                    <input type="text" class="form-control form-control-lg border-0 edit-todo-input rounded px-3 d-none" value="Renew car insurance" />
+                </div>
+                <div class="col-auto m-1 p-0 px-3">
+                    <div class="row">
+                        <div class="col-auto d-flex align-items-center rounded bg-white border border-warning">
+                            <i class="fa fa-hourglass-2 my-2 px-2 text-warning btn" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Due on date"></i>
+                            <h6 class="text my-2 pr-2">28th Jun 2020</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto m-1 p-0 todo-actions">
+                    <div class="row d-flex align-items-center justify-content-end">
+                        <h5 class="m-0 p-0 px-2">
+                            <i class="fa fa-pencil text-info btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
+                        </h5>
+                        <h5 class="m-0 p-0 px-2">
+                            <i class="fa fa-trash-o text-danger btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
+                        </h5>
+                    </div>
+                    <div class="row todo-created-info">
+                        <div class="col-auto d-flex align-items-center pr-2">
+                            <i class="fa fa-info-circle my-2 px-2 text-black-50 btn" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
+                            <label class="date-label my-2 text-black-50">28th Jun 2020</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Todo Item 3 -->
+            <div class="row px-3 align-items-center todo-item editing rounded">
+                <div class="col-auto m-1 p-0 d-flex align-items-center">
+                    <h2 class="m-0 p-0">
+                        <i class="fa fa-square-o text-primary btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Mark as complete"></i>
+                        <i class="fa fa-check-square-o text-primary btn m-0 p-0 d-none" data-toggle="tooltip" data-placement="bottom" title="Mark as todo"></i>
+                    </h2>
+                </div>
+                <div class="col px-1 m-1 d-flex align-items-center">
+                    <input type="text" class="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3 d-none" readonly value="Sign up for online course" title="Sign up for online course" />
+                    <input type="text" class="form-control form-control-lg border-0 edit-todo-input rounded px-3" value="Sign up for online course" />
+                </div>
+                <div class="col-auto m-1 p-0 px-3 d-none">
+                </div>
+                <div class="col-auto m-1 p-0 todo-actions">
+                    <div class="row d-flex align-items-center justify-content-end">
+                        <h5 class="m-0 p-0 px-2 edit-icon">
+                            <i class="fa fa-pencil text-info btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Edit todo"></i>
+                        </h5>
+                        <h5 class="m-0 p-0 px-2">
+                            <i class="fa fa-trash-o text-danger btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
+                        </h5>
+                    </div>
+                    <div class="row todo-created-info">
+                        <div class="col-auto d-flex align-items-center pr-2">
+                            <i class="fa fa-info-circle my-2 px-2 text-black-50 btn" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Created date"></i>
+                            <label class="date-label my-2 text-black-50">28th Jun 2020</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   </main>
 </template>
+
+<style scoped>
+.add-todo-input,
+.edit-todo-input {
+    outline: none;
+}
+
+.add-todo-input:focus,
+.edit-todo-input:focus {
+    border: none !important;
+    box-shadow: none !important;
+}
+
+.view-opt-label,
+.date-label {
+    font-size: 0.8rem;
+}
+
+.edit-todo-input {
+    font-size: 1.7rem !important;
+}
+
+.todo-actions {
+    visibility: hidden !important;
+}
+
+.todo-item:hover .todo-actions {
+    visibility: visible !important;
+}
+
+.todo-item.editing .todo-actions .edit-icon {
+    display: none !important;
+}
+</style>
